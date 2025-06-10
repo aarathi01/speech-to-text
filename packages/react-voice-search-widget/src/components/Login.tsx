18f663efa3b82736a60f63e39374185f343553a7
aarathi-aspiresys <aarathi.ajith@aspiresys.com>
2025-06-10 13:13:58 +0530

    FE : added Login and register user

--- packages/react-voice-search-widget/src/components/Login.tsx
+++ packages/react-voice-search-widget/src/components/Login.tsx
@@ -5,42 +5,65 @@ import axios from "axios";
 const Login: React.FC = () => {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
+  const [isRegistering, setIsRegistering] = useState(false);
   const navigate = useNavigate();
+  const BASE_URL = import.meta.env.VITE_BASE_URL;
 
-  const handleLogin = async (e: React.FormEvent) => {
-    e.preventDefault();
+  const handleLogin = async () => {
     try {
-      const res = await axios.post("http://localhost:5000/auth/login", {
+      const res = await axios.post(`${BASE_URL}/auth/login`, {
         username,
         password,
       });
       localStorage.setItem("token", res.data.token);
-      navigate("/", { replace: true });
-    }  catch (err: unknown) {
-      console.error("Invalid username or password", err);
-      alert("Invalid username or password");
+      navigate("/");
+    } catch (err: unknown) {
+      const errorMsg = err instanceof Error ? err.message : "Login failed";
+      alert(`Login Failed: ${errorMsg}`);
+    }
+  };
+
+  const handleRegister = async () => {
+    try {
+      const res = await axios.post(`${BASE_URL}/auth/register`, {
+        username,
+        password,
+      });
+      localStorage.setItem("token", res.data.token);
+      alert("Registration Successfull.");
+    } catch (err: unknown) {
+      const errorMsg =
+        err instanceof Error ? err.message : "Registration failed";
+      alert(`Registration Failed: ${errorMsg}`);
     }
   };
 
   return (
-    <form onSubmit={handleLogin}>
-      <h2>Login</h2>
+    <div className="login-container">
+      <h2>{isRegistering ? "Register" : "Login"}</h2>
       <input
         type="text"
         placeholder="Username"
         value={username}
         onChange={(e) => setUsername(e.target.value)}
-        required
       />
       <input
         type="password"
         placeholder="Password"
         value={password}
         onChange={(e) => setPassword(e.target.value)}
-        required
       />
-      <button type="submit">Login</button>
-    </form>
+      {isRegistering ? (
+        <button onClick={handleRegister}>Register</button>
+      ) : (
+        <button onClick={handleLogin}>Login</button>
+      )}
+      <p className="toggle" onClick={() => setIsRegistering(!isRegistering)}>
+        {isRegistering
+          ? "Already have an account? Login"
+          : "Don't have an account? Register"}
+      </p>
+    </div>
   );
 };
 
