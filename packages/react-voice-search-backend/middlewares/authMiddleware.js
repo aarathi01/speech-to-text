e7cc31edfc30c2a4ae5e007fab6fa42dee4b7ab5
aarathi-aspiresys <aarathi.ajith@aspiresys.com>
2025-06-13 14:58:00 +0530

    BE : Unnecessary decoded values should be removed

--- packages/react-voice-search-backend/middlewares/authMiddleware.js
+++ packages/react-voice-search-backend/middlewares/authMiddleware.js
@@ -10,7 +10,8 @@ export const authMiddleware = (req, res, next) => {
   const token = authHeader.split(" ")[1];
   try {
     const decoded = jwt.verify(token, JWT_SECRET);
-    req.user = decoded;
+    req.userId = decoded.id; //req.user = decoded full JWT payload
+    // Later to support user-specific search history or want to log who transcribed something
     next();
   } catch (err) {
     return res.status(403).json({ error: "Invalid or expired token" });
