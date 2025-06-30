41229158290915db13cb837c19008dab441a440d
aarathi-aspiresys <aarathi.ajith@aspiresys.com>
2025-06-30 11:27:01 +0530

    BE :  updated for error in search querry

--- packages/react-voice-search-backend/validations/searchValidation.js
+++ packages/react-voice-search-backend/validations/searchValidation.js
@@ -1,7 +1,7 @@
 import Joi from "joi";
 
 export const searchSchema = Joi.object({
-  query: Joi.string().trim().min(1).required().messages({
+  q: Joi.string().trim().min(1).required().messages({
     "string.empty": "Search query is required",
     "any.required": "Search query is required",
   }),
